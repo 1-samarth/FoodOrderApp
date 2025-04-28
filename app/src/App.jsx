c5d9e7214a0fed9d8351dba8b1ed975c9c96e74a
1-samarth <samarthchandel587@gmail.com>
2025-04-28 23:56:01 +0530

    final change

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import SearchResult from "./components/SearchResults/SearchResult";
 
-export const BASE_URL = "https://foodorderappfrontend.onrender.com/";
+export const BASE_URL = "https://foodorderapp-rnmx.onrender.com/";
 
 const App = () => {
   const [data, setData] = useState(null);
